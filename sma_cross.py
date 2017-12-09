ed85984eccce8fb43982d7c53e8c160efbfa51f1
haimin777 <haimin777@yandex.ru>
2017-12-09 20:56:09 +0300

    trade logic func

--- sma_cross.py
+++ sma_cross.py
@@ -4,7 +4,7 @@ Created on Mon Oct 30 18:16:20 2017
 
 @author: Haimin
 """
-import time
+
 import histData  # через него получаем исторические данные как датафрейм
 
 from order import OrderIB
@@ -31,25 +31,23 @@ class account_ib:
         self.list1_orders = []  # Открытые ордера: Тикер и цена
         self.pos_list = []  # Активные позиции: тикер, размер позиции, рыночная стоимость, цена
         self.list2_orders = list()  # Открытые ордера: Объем и статус
-        self.signal = None  # Предыдущее значение сигнала на вход
         self.current_order_status = None
 
+    def position_handler(self, msg):
 
-    def position_handler(self,msg):
-
-        if  msg.typeName == "nextValidId":
+        if msg.typeName == "nextValidId":
             print("order id = ", msg.orderId)
             self.order_ID = msg.orderId
 
         elif msg.typeName == "orderStatus":
 
             print("___________order placed__________", "\n", "id: ",
-                    msg.orderId, "order status: ", msg.status)
+                  msg.orderId, "order status: ", msg.status)
             self.current_order_status = msg.status
             sleep(1)
 
-        #elif msg.typeName == "orderStatus" and msg.orderId == self.order_ID-1:
-        #    print("___----___")
+            #elif msg.typeName == "orderStatus" and msg.orderId == self.order_ID-1:
+            #print("___----___")
 
     def server_handler(self, msg):
         #print("Server Msg:", msg.typeName, "-", msg) # Для отладки выводим все сообщения системы
@@ -99,12 +97,11 @@ class account_ib:
         self.list2_orders.clear()
         self.pos_list.clear()
 
-    def operation_status(self): # выводим статус заявки, созданной алгоритмом
+    def operation_status(self):  # выводим статус заявки, созданной алгоритмом
 
         print("______new order signal_______", "\n")
         self.tws_conn.reqOpenOrders()
         self.tws_conn.registerAll(self.position_handler)
-
 
     def connect_to_tws(self):
         self.tws_conn = Connection.create(port=self.port,
@@ -128,24 +125,25 @@ class account_ib:
     def register_callback_functions(self):
         # Assign server messages handling function.
         self.tws_conn.registerAll(self.server_handler)
-       # self.tws_conn.registerAll(self.position_handler)
+        # self.tws_conn.registerAll(self.position_handler)
         # Assign error handling function.
         self.tws_conn.register(self.error_handler, 'Error')
 
-    def cross_signal(self, data_loader):   #функция для определения пересечения
+    def cross_signal(self, data_loader):  # функция для определения пересечения
         self.cur_time = datetime.datetime.now().strftime("%Y%m%d %H:%M:%S") + str(
             " GMT")  # запрашиваем текущее время в нужном формате
         self.contract = OrderIB.create_contract("EUR", "CASH", "IDEALPRO", "USD")
-        data = data_loader.requestData(self.contract, self.cur_time, '1 D','5 mins', )  # запрашиваем данные для 5 минутных свеч
+        data = data_loader.requestData(self.contract, self.cur_time, '1 D',
+                                       '5 mins', )  # запрашиваем данные для 5 минутных свеч
         ma_long = talib.SMA(data.close.values, timeperiod=80)[-1]  # вычисляем SMA
         ma_short = talib.SMA(data.close.values, timeperiod=10)[-1]
 
         allow = ma_short > ma_long
         return allow
-    
+
     def trade_logic(self, data_loader, pos_volume):
 
-        allow = self.cross_signal(data_loader) # Проверяем сигнал на вход
+        allow = self.cross_signal(data_loader)  # Проверяем сигнал на вход
 
         if allow:  # проверка пересечения
 
@@ -154,9 +152,9 @@ class account_ib:
             if self.position == 0:
                 self.ib_order = OrderIB.create_order('MKT', pos_volume, 'BUY')
 
-            elif self.position < 0:  #     выставляем ордер с учетом перекрытия текущей позиции
+            elif self.position < 0:  # выставляем ордер с учетом перекрытия текущей позиции
 
-                self.ib_order = OrderIB.create_order("MKT", abs(self.position)+pos_volume, "BUY")
+                self.ib_order = OrderIB.create_order("MKT", abs(self.position) + pos_volume, "BUY")
 
             return self.ib_order
 
@@ -167,7 +165,7 @@ class account_ib:
                 self.ib_order = OrderIB.create_order('MKT', pos_volume, 'SELL')
             elif self.position > 0:
                 # перворачиваем текущую длинную позицию
-                self.ib_order = OrderIB.create_order("MKT", abs(self.position)+pos_volume, "SELL")
+                self.ib_order = OrderIB.create_order("MKT", abs(self.position) + pos_volume, "SELL")
             return self.ib_order
 
     def start(self, sec):
@@ -186,18 +184,14 @@ class account_ib:
             self.monitor_position()
             sleep(1)
 
-
             while True:
-
                 tws.registerAll(self.position_handler)
                 self.register_callback_functions()
                 # Расчитаем количество лотов, как баланс в тысячах (заходим на весь баланс без рычага)
-                #pos_volume = 9000
                 pos_volume = int(float(self.balance) // 1000 * 1000)
-                print("\n", "last placed order status:", "\n",self.current_order_status)
+                print("\n", "last placed order status:", "\n", self.current_order_status)
                 # Алгоритм системы
                 self.trade_logic(data_loader, pos_volume)
-                #if self.current_order_status != "Filled" and self.current_order_status != None: #проверяем статус созданного ордера
                 tws.placeOrder(self.order_ID, self.contract, self.ib_order)
 
                 sleep(self.sec)
@@ -206,8 +200,8 @@ class account_ib:
 
             # выводим данные при выходе
 
-            print("\n",'Position:%s Bal:%s  Open orders:%s' % (self.position,
-                                                          self.balance, self.OpenOrders))
+            print("\n", 'Position:%s Bal:%s  Open orders:%s' % (self.position,
+                                                                self.balance, self.OpenOrders))
             print("disconnected")
             self.disconnect_from_tws()
 
