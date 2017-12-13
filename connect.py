3a7a6a52612deee29cf63e6fa1913a79761cdf8c
haimin777 <haimin777@yandex.ru>
2017-12-13 15:50:41 +0300

    connect added

--- connect.py
+++ connect.py
@@ -6,33 +6,48 @@ Created on Mon Oct 30 17:34:25 2017
 """
 
 from ib.opt import Connection
+from time import sleep
+
+class connect_ib:
+    def __init__(self):
+        self.client_id = 100
+        self.port = 7496
+        self.tws_conn = None
+
+    def error_handler(self, msg):
+        print ("Server Error:", msg)
 
 
-def error_handler(msg):
-    print ("Server Error:", msg)
+    def server_handler(self, msg):
+        print ("Server Msg:", msg.typeName, "-", msg)
 
 
-def server_handler(msg):
-    print ("Server Msg:", msg.typeName, "-", msg)
+#if __name__ == "__main__":
+#    client_id = 100
+#    order_id = 1
+#    port = 7496
+#    tws_conn = None
+    
+    def connect(self):
+        try:
+            self.tws_conn = Connection.create(port=self.port,
+                                     clientId=self.client_id)
+            self.tws_conn.connect()
+    # Assign error handling function.
+            self.tws_conn.register(self.error_handler, 'Error')
+    # Assign server messages handling function.
+            self.tws_conn.registerAll(self.server_handler)
+         #   print("Connection Ok")
+        finally:
+            print("Connection Ok")
 
+    def disconnect(self):
+        if self.tws_conn is not None:
+            self.tws_conn.disconnect()
+            print("Disconnect OK")
 
 if __name__ == "__main__":
-    client_id = 100
-    order_id = 1
-    port = 7496
-    tws_conn = None
-    
-try:
-    # Establish connection to TWS.
-    tws_conn = Connection.create(port=port,
-                                 clientId=client_id)
-    tws_conn.connect()
-    # Assign error handling function.
-    tws_conn.register(error_handler, 'Error')
-    # Assign server messages handling function.
-    tws_conn.registerAll(server_handler)
-    print("Connection Ok")
-finally:
-    # Disconnect from TWS
-    if tws_conn is not None:
-        tws_conn.disconnect()
+    system = connect_ib()
+    system.connect()
+    sleep(1)
+    system.disconnect()
